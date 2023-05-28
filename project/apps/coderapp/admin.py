c7dd95d2cc423b35c38ba6767f9587e41c1e162a
jrglpz <113955091+jrglpz@users.noreply.github.com>
2023-05-28 01:09:23 -0300

    update admin.py , create readme.md en project

--- project/apps/coderapp/admin.py
+++ project/apps/coderapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import CreaAlumno, CreaProfesor, CreaCurso
 # Register your models here.
+admin.site.register(CreaAlumno)
+admin.site.register(CreaProfesor)
+admin.site.register(CreaCurso)
